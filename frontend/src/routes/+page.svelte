a4ce9ed1b0b195b6960ecd7c36f3dd349a54d53a
guidesify <64188976+guidesify@users.noreply.github.com>
2023-06-09 16:58:46 +0800

    Start of Frontend

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,59 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/title.svg';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Machine Learning Engineering - Crisis Dectection Demo</title>
+	<meta name="description" content="Machine Learning Engineering - Crisis Dectection Demo" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="welcome">
+			<img src={welcome} alt="Guidesify Logo" />
+		</span>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+    position: absolute;
+    width: auto;
+    height: auto;
+    max-width: 50%;
+    max-height: 100%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
