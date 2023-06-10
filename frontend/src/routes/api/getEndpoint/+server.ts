2c20d62a4e16c97cf486b80e688bb0aa4dc37519
guidesify <64188976+guidesify@users.noreply.github.com>
2023-06-10 13:00:52 +0800

    Changing to sdk v3

--- frontend/src/routes/api/getEndpoint/+server.ts
+++ frontend/src/routes/api/getEndpoint/+server.ts
@@ -1,4 +1,5 @@
 import AWS from 'aws-sdk';
+import AWS_SageMaker, { SageMaker } from "@aws-sdk/client-sagemaker";
 import { AWS_KEY, AWS_SECRET } from '$env/static/private';
 import type { RequestHandler } from './$types';
 import { json } from '@sveltejs/kit'
@@ -14,16 +15,16 @@ export const POST: RequestHandler = async ({ request }) => {
 
   const region = 'us-east-1'; // Replace with your actual AWS region
 
-  const sagemaker = new AWS.SageMaker({ credentials, region });
+  const sagemaker = new SageMaker({ credentials, region });
 
-  const listEndpointsParams: AWS.SageMaker.ListEndpointsInput = {
+  const listEndpointsParams: AWS_SageMaker.ListEndpointsCommandInput = {
     SortBy: 'CreationTime', // Optional: Specify a sorting order
     SortOrder: 'Descending', // Optional: Specify the sorting order (e.g., 'Ascending', 'Descending')
     MaxResults: 10, // Optional: Specify the maximum number of endpoints to retrieve
   };
 
   try {
-    const response = await sagemaker.listEndpoints(listEndpointsParams).promise();
+    const response = await sagemaker.listEndpoints(listEndpointsParams);
     console.log(response.Endpoints.length);
     return json(response.Endpoints);
   } catch (error) {
