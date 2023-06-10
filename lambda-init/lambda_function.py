7543007a4efe61a8d6d521eb6970e47af0c8882a
alphevalle <94121835+alphevalle@users.noreply.github.com>
2023-06-10 14:22:20 +0800

    latest lambda function to include base training step

--- lambda-init/lambda_function.py
+++ lambda-init/lambda_function.py
@@ -1,22 +1,38 @@
 import boto3
 import pandas as pd
-from init_proj import init_data
+from init_proj import init_proj
+
+# import sagemaker
 
 def lambda_handler(event, context):
 
     # Store the preprocessed data in S3
     s3 = boto3.client('s3')
     bucket_name = 'crisis-detection-bucket-1' # <--- CHANGE THIS VARIABLE TO A UNIQUE NAME FOR YOUR BUCKET
+
     file_name = 'base.csv'
     
     dataset = pd.read_csv('data.csv')
-    df = init_data(dataset)
+    df = init_proj(dataset)
     
     # Convert DataFrame to CSV
     preprocessed_data_csv = df.to_csv(index=False)
     
     # Upload the file to S3
     s3.put_object(Body=preprocessed_data_csv, Bucket=bucket_name, Key=file_name)
+
+    # sagemaker_session = boto3.client('sagemaker')
+    print('1')
+    session = boto3.Session()
+    sts_client = session.client('sts')
+    response = sts_client.get_caller_identity()
+    print('2)')
+    role_arn = response['Arn']
+    sagemaker_session = session.client('sagemaker', role_arn=role_arn)
+    print('3')
+    role = sagemaker_session.get_execution_role()
+    base_model = train_model('train.py', ['utils.py'], 's3://crisis-detection-bucket-1/base.csv', role, sagemaker_session, bucket_name)
+    print('4')
 
     return {
         'statusCode': 200,
