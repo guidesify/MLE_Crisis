396204bdc79c8c0c3ba46b916cdaf8d11e4a2e9f
guidesify <64188976+guidesify@users.noreply.github.com>
2023-06-13 01:08:50 +0800

    Error with Typescript

--- frontend/src/routes/api/getEndpoint/+server.ts
+++ frontend/src/routes/api/getEndpoint/+server.ts
@@ -1,12 +1,10 @@
 import AWS from 'aws-sdk';
-import AWS_SageMaker, { SageMaker } from "@aws-sdk/client-sagemaker";
+import { SageMaker } from "@aws-sdk/client-sagemaker";
 import { AWS_KEY, AWS_SECRET } from '$env/static/private';
-import type { RequestHandler } from './$types';
-import { json } from '@sveltejs/kit'
+import { json } from '@sveltejs/kit';
 
-
-export const POST: RequestHandler = async ({ request }) => {
-  let {endpoint} = await request.json();
+export const POST = async ({ request }) => {
+  let { endpoint } = await request.json();
   // console.log(endpoint)
   const credentials = new AWS.Credentials({
     accessKeyId: AWS_KEY,
@@ -17,7 +15,7 @@ export const POST: RequestHandler = async ({ request }) => {
 
   const sagemaker = new SageMaker({ credentials, region });
 
-  const listEndpointsParams: AWS_SageMaker.ListEndpointsCommandInput = {
+  const listEndpointsParams = {
     SortBy: 'CreationTime', // Optional: Specify a sorting order
     SortOrder: 'Descending', // Optional: Specify the sorting order (e.g., 'Ascending', 'Descending')
     MaxResults: 10, // Optional: Specify the maximum number of endpoints to retrieve
@@ -31,4 +29,4 @@ export const POST: RequestHandler = async ({ request }) => {
     console.error('Error retrieving SageMaker endpoints:', error);
     throw error;
   }
-}
+};
