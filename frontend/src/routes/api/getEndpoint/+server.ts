3cbd4d0d393be70bbfea0f1a28c058ffe59eac5d
guidesify <64188976+guidesify@users.noreply.github.com>
2023-06-13 01:32:04 +0800

    Update +server.ts

--- frontend/src/routes/api/getEndpoint/+server.ts
+++ frontend/src/routes/api/getEndpoint/+server.ts
@@ -6,27 +6,28 @@ import { json } from '@sveltejs/kit';
 export const POST = async ({ request }) => {
   let { endpoint } = await request.json();
   // console.log(endpoint)
-  const credentials = new AWS.Credentials({
-    accessKeyId: AWS_KEY,
-    secretAccessKey: AWS_SECRET,
-  });
+  // const credentials = new AWS.Credentials({
+  //   accessKeyId: AWS_KEY,
+  //   secretAccessKey: AWS_SECRET,
+  // });
 
-  const region = 'us-east-1'; // Replace with your actual AWS region
+  // const region = 'us-east-1'; // Replace with your actual AWS region
 
-  const sagemaker = new SageMaker({ credentials, region });
+  // const sagemaker = new SageMaker({ credentials, region });
 
-  const listEndpointsParams = {
-    SortBy: 'CreationTime', // Optional: Specify a sorting order
-    SortOrder: 'Descending', // Optional: Specify the sorting order (e.g., 'Ascending', 'Descending')
-    MaxResults: 10, // Optional: Specify the maximum number of endpoints to retrieve
-  };
+  // const listEndpointsParams = {
+  //   SortBy: 'CreationTime', // Optional: Specify a sorting order
+  //   SortOrder: 'Descending', // Optional: Specify the sorting order (e.g., 'Ascending', 'Descending')
+  //   MaxResults: 10, // Optional: Specify the maximum number of endpoints to retrieve
+  // };
 
-  try {
-    const response = await sagemaker.listEndpoints(listEndpointsParams);
-    // console.log(response.Endpoints.length);
-    return json(response.Endpoints);
-  } catch (error) {
-    console.error('Error retrieving SageMaker endpoints:', error);
-    throw error;
-  }
+  // try {
+  //   const response = await sagemaker.listEndpoints(listEndpointsParams);
+  //   // console.log(response.Endpoints.length);
+  //   return json(response.Endpoints);
+  // } catch (error) {
+  //   console.error('Error retrieving SageMaker endpoints:', error);
+  //   throw error;
+  // }
+  return json([{"EndpointName": "test-endpoint", "EndpointStatus": "InService"}])
 };
