f33884d98b6d6b04cb540a4f38c58f5437fe9d79
guidesify <64188976+guidesify@users.noreply.github.com>
2023-06-15 06:10:10 +0800

    KIV fix crypto

--- frontend/src/routes/api/getPredictions/+server.ts
+++ frontend/src/routes/api/getPredictions/+server.ts
@@ -1,5 +1,6 @@
-import { SageMakerRuntimeClient, InvokeEndpointCommand } from "@aws-sdk/client-sagemaker-runtime";
-import { SageMaker } from "@aws-sdk/client-sagemaker";
+import axios from 'axios';
+import CryptoJS from 'crypto-js';
+
 import { AWS_KEY, AWS_SECRET } from '$env/static/private';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
@@ -9,40 +10,38 @@ export const POST: RequestHandler = async ({ request }) => {
     let { endpoint, tweets } = await request.json();
     const region = 'us-east-1'; // Replace with your actual AWS region
   
-    const sagemaker =  new SageMaker({
-        region,
-        credentials: {
-          accessKeyId: AWS_KEY,
-          secretAccessKey: AWS_SECRET,
-        },
-      });
-
-    // Pass credentials from sagemaker to runtime
-    const runtime = new SageMakerRuntimeClient({
-        region,
-        credentials: sagemaker.config.credentials,
-    });
-
     // Construct the request parameters
     const params = {
-        EndpointName: endpoint,
-        Body: JSON.stringify({ Input: tweets }),
-        ContentType: "application/json",
+      EndpointName: endpoint,
+      Body: JSON.stringify({ Input: tweets }),
+      ContentType: 'application/json',
     };
 
-        // Make the inference request
-        const command = new InvokeEndpointCommand(params);
-        const response = await runtime.send(command);
+    // Generate the AWS signature
+    const date = new Date().toISOString().replace(/[:\-]|\.\d{3}/g, '');
+    const canonicalRequest = `${request.method}\n${request.path}\n\nhost:${request.host}\nx-amz-date:${date}\n\nhost;x-amz-date\n${CryptoJS.SHA256(JSON.stringify(params)).toString(CryptoJS.enc.Hex)}`;
+    const stringToSign = `AWS4-HMAC-SHA256\n${date}\n${date.substr(0, 8)}/${region}/sagemaker/aws4_request\n${CryptoJS.SHA256(canonicalRequest).toString(CryptoJS.enc.Hex)}`;
+    const signingKey = CryptoJS.HmacSHA256(date.substr(0, 8), CryptoJS.HmacSHA256(region, CryptoJS.HmacSHA256('sagemaker', CryptoJS.HmacSHA256('aws4_request', `AWS4${AWS_SECRET}`))));
+    const signature = CryptoJS.HmacSHA256(stringToSign, signingKey).toString(CryptoJS.enc.Hex);
+    const authorizationHeader = `AWS4-HMAC-SHA256 Credential=${AWS_KEY}/${date.substr(0, 8)}/${region}/sagemaker/aws4_request, SignedHeaders=host;x-amz-date, Signature=${signature}`;
 
-        // Parse the predictions from the response
-        const { Body } = response;
-        const predictionData = JSON.parse(Buffer.from(Body).toString());
-
-        // Render the page template with the predictions
-        console.log("Number of Predictions:", predictionData.Output.length);
-        return json(predictionData);
-    } catch (error) {
-        console.error("Error making inference request:", error);
-        return json(error);
-    }
+    // Make the inference request
+    const url = `https://runtime.sagemaker.${region}.amazonaws.com/endpoints/${endpoint}/invocations`;
+    const headers = {
+      'Content-Type': 'application/json',
+      'X-Amz-Date': date,
+      'Authorization': authorizationHeader,
     };
+    const response = await axios.post(url, params.Body, { headers });
+
+    // Parse the predictions from the response
+    const predictionData = JSON.parse(response.data);
+
+    // Render the page template with the predictions
+    console.log('Number of Predictions:', predictionData.Output.length);
+    return json(predictionData);
+  } catch (error) {
+    console.error('Error making inference request:', error);
+    return json(error);
+  }
+};
