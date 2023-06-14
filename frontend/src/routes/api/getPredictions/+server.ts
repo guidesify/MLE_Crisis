7d8fa55551a6e9428ec64e8f09ba8a707cef3505
guidesify <64188976+guidesify@users.noreply.github.com>
2023-06-15 04:24:42 +0800

    Try alternative

--- frontend/src/routes/api/getPredictions/+server.ts
+++ frontend/src/routes/api/getPredictions/+server.ts
@@ -1,5 +1,5 @@
-import { SageMakerRuntime, InvokeEndpointCommand } from "@aws-sdk/client-sagemaker-runtime";
 import { AWS_KEY, AWS_SECRET } from '$env/static/private';
+import AWS from 'aws-sdk';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
@@ -9,40 +9,35 @@ export const POST: RequestHandler = async ({ request }) => {
 
     const region = 'us-east-1'; // Replace with your actual AWS region
 
-    // Make a request to SageMaker for predictions
-    const sagemaker = new SageMakerRuntime({
-        region,
-        credentials: {
-            accessKeyId: AWS_KEY,
-            secretAccessKey: AWS_SECRET,
-        },
-      });
+    // Set up the AWS credentials
+    AWS.config.update({
+      accessKeyId: AWS_KEY,
+      secretAccessKey: AWS_SECRET,
+      region: region
+    });
+
+    // Create a SageMaker runtime instance
+    const sagemaker = new AWS.SageMakerRuntime();
 
     // Construct the request parameters
     const params = {
-        EndpointName: endpoint,
-        Body: JSON.stringify({ Input: tweets }),
-        ContentType: "application/json",
+      EndpointName: endpoint,
+      Body: JSON.stringify({ Input: tweets }),
+      ContentType: "application/json",
     };
 
-        // Make the inference request
-        const command = new InvokeEndpointCommand(params);
-        const response = await sagemaker.send(command);
+    // Make the inference request
+    const response = await sagemaker.invokeEndpoint(params).promise();
 
-        // Parse the predictions from the response
-        const { Body } = response;
-        const predictionData = JSON.parse(Buffer.from(Body).toString());
+    // Parse the predictions from the response
+    const { Body } = response;
+    const predictionData = JSON.parse(Buffer.from(Body as ArrayBuffer).toString());
 
-        // Render the page template with the predictions
-        console.log("Number of Predictions:", predictionData.Output.length);
-        return json(predictionData);
-    } catch (error) {
-        console.error("Error making inference request:", error);
-        return json(error)
-    }
-    };
-  
-
-
-
-
+    // Render the page template with the predictions
+    console.log("Number of Predictions:", predictionData.Output.length);
+    return json(predictionData);
+  } catch (error) {
+    console.error("Error making inference request:", error);
+    return json(error);
+  }
+};
