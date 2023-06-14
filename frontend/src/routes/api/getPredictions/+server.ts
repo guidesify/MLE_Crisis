c5228a4d2a75967f318d88ba00863d1a32c772f4
guidesify <64188976+guidesify@users.noreply.github.com>
2023-06-15 05:42:37 +0800

    Try

--- frontend/src/routes/api/getPredictions/+server.ts
+++ frontend/src/routes/api/getPredictions/+server.ts
@@ -1,49 +1,49 @@
-import { json } from '@sveltejs/kit';
+import { SageMakerRuntimeClient, InvokeEndpointCommand } from "@aws-sdk/client-sagemaker-runtime";
+import { SageMaker } from "@aws-sdk/client-sagemaker";
 import { AWS_KEY, AWS_SECRET } from '$env/static/private';
-import aws4 from 'aws4';
-import fetch from 'node-fetch';
+import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ request }) => {
-  try {
     let { endpoint, tweets } = await request.json();
-
     const region = 'us-east-1'; // Replace with your actual AWS region
+  
+    const sagemaker =  new SageMaker({
+        region,
+        credentials: {
+          accessKeyId: AWS_KEY,
+          secretAccessKey: AWS_SECRET,
+        },
+      });
+
+
+    // Pass credentials from sagemaker to runtime
+    const runtime = new SageMakerRuntimeClient({
+        region,
+        credentials: sagemaker.config.credentials,
+    });
 
     // Construct the request parameters
     const params = {
-      host: `runtime.sagemaker.${region}.amazonaws.com`,
-      path: `/endpoints/${endpoint}/invocations`,
-      service: 'sagemaker',
-      region: region,
-      body: JSON.stringify({ Input: tweets }),
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
+        EndpointName: endpoint,
+        Body: JSON.stringify({ Input: tweets }),
+        ContentType: "application/json",
     };
 
-    // Sign the request using aws4
-    const signedRequest = aws4.sign(params, {
-      accessKeyId: AWS_KEY,
-      secretAccessKey: AWS_SECRET,
-    });
+    try {
+        // Make the inference request
+        const command = new InvokeEndpointCommand(params);
+        const response = await runtime.send(command);
 
-    // Make the inference request using node-fetch
-    const response = await fetch(`https://${params.host}${params.path}`, {
-      method: signedRequest.method,
-      headers: signedRequest.headers,
-      body: signedRequest.body,
-    });
+        // Parse the predictions from the response
+        const { Body } = response;
+        const predictionData = JSON.parse(Buffer.from(Body).toString());
 
-    // Parse the predictions from the response
-    const predictionData = await response.json();
-
-    // Render the page template with the predictions
-    console.log("Number of Predictions:", predictionData.Output.length);
-    return json(predictionData);
-  } catch (error) {
-    console.error("Error making inference request:", error);
-    return json(error);
-  }
-};
+        // Render the page template with the predictions
+        console.log("Number of Predictions:", predictionData.Output.length);
+        return json(predictionData);
+    } catch (error) {
+        console.error("Error making inference request:", error);
+        throw error;
+    }
+    };
